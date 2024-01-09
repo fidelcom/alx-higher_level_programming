4f4531378b465ddf73758ed0ff3c6df5fc16b6a9
fidelcom <okpara2040@gmail.com>
2024-01-09 14:15:16 +0000

    Write a function that multiplies 2 matrices by using the module NumPy

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function using NumPy."""
-import numpy as np
+"""
+Module 5-text_indentation
+Contains method that prints text with 2 new lines after each ".", "?", and ":"
+Takes in a string
+"""
 
 
-def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
-
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
+def text_indentation(text):
     """
+    Prints text with 2 new lines after each ".", "?", and ":"
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
-    return (np.matmul(m_a, m_b))
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    list_lines = [lines.strip(' ') for lines in text.split('\n')]
+    revised = "\n".join(list_lines)
+    print(revised, end="")
